21875d6c2200ea779b0571bd09416352682b1da9
ck0jo <ck0jo@ck0jo.ck0jo>
2024-03-21 20:47:50 +0100

    Add basic structure

---
+++ Makefile
@@ -0,0 +1,32 @@
+# Makefile variables
+CONTAINER_PHP = app
+CONTAINER_DB = db
+CONTAINER_DB_TEST = db_test
+
+.PHONY: up down restart logs shell db-shell db-test-shell migrate test
+
+up:
+	@docker-compose up -d
+
+down:
+	@docker-compose down
+
+restart: down up
+
+logs:
+	@docker-compose logs -f
+
+shell:
+	@docker-compose exec $(CONTAINER_PHP) /bin/bash
+
+db-shell:
+	@docker-compose exec $(CONTAINER_DB) mysql -u root -p
+
+db-test-shell:
+	@docker-compose exec $(CONTAINER_DB_TEST) mysql -u root -p
+
+migrate:
+	@docker-compose exec $(CONTAINER_PHP) php artisan migrate
+
+test:
+	@docker-compose exec $(CONTAINER_PHP) php artisan test
---
+++ database/migrations/2024_03_21_185341_create_urls_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('original_ur;');
+            $table->string('short_code', 6)->unique();
+            $table->unsignedBigInteger('clicks')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('urls');
+    }
+};
