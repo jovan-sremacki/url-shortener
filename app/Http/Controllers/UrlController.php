c46c0f79301f57568dc267fc7fa6654579d84d7a
ck0jo <ck0jo@ck0jo.ck0jo>
2024-03-21 19:21:52 +0100

    Add Laravel project

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class UrlController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    // public function store(UrlStore)
+}
