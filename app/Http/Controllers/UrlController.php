d1b0319225607ea2b7c939041f3447be5fedfdc5
Jovan Sremacki <jovansr@protonmail.com>
2024-03-22 08:50:25 +0100

    Add logic for basic short URL creation

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -2,13 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use App\Models\ShortUrl;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
-class UrlController extends BaseController
+class UrlController extends Controller
 {
-    use AuthorizesRequests, ValidatesRequests;
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'url' => 'required|url'
+        ]);
 
-    // public function store(UrlStore)
+        $originalUrl = $validatedData['url'];
+
+        // Google Safe Browsing API logic
+
+        $existingShortUrl = ShortUrl::where('original_url', $originalUrl)->first();
+
+        if($existingShortUrl) {
+            return response()->json([
+                'short_url' => url("/{$existingShortUrl->short_code}")
+            ]);
+        }
+
+        do {
+            $hash = Str::random(6);
+        } while (ShortUrl::where('short_code', $hash)->exists());
+
+        $shortUrl = ShortUrl::create([
+            'original_url' => $originalUrl,
+            'short_code' => $hash
+        ]);
+
+        return response()->json([
+            'short_url' => url("/{$shortUrl->short_code}")
+        ]);
+    }
 }
