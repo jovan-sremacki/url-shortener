6a096e935f8db22e4570d82f2c64d5aa93929569
ck0jo <ck0jo@ck0jo.ck0jo>
2024-03-25 14:30:35 +0100

    DEV-4: Add basic structure for VueJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import Example from "./components/Example.vue"
+
+const app = createApp({
+    components: {
+        Example
+    }
+});
+
+app.mount("#app");
